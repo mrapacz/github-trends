6e76b3a4612690176aa45cdbabecf91dfe8b08ca
Maciej Rapacz <mrapacz@users.noreply.github.com>
2017-05-06 17:11:07 +0200

    Add oauth (#2)

---
+++ github_trends/web/oauth/github.ex
@@ -0,0 +1,37 @@
+defmodule Github do
+  use OAuth2.Strategy
+
+  alias OAuth2.Strategy.AuthCode
+
+  def client do
+    OAuth2.Client.new([
+      strategy: __MODULE__,
+      client_id: System.get_env("CLIENT_ID"),
+      client_secret: System.get_env("CLIENT_SECRET"),
+      redirect_uri: System.get_env("REDIRECT_URI"),
+      site: "https://api.github.com",
+      authorize_url: "https://github.com/login/oauth/authorize",
+      token_url: "https://github.com/login/oauth/access_token"
+    ])
+  end
+
+  def authorize_url!(params \\ []) do
+    OAuth2.Client.authorize_url!(client(), scope: "user,public_repo")
+  end
+
+  def get_token!(params \\ [], headers \\ []) do
+    OAuth2.Client.get_token!(client(), Keyword.merge(params, client_secret: client().client_secret))
+  end
+
+  # strategy callbacks
+
+  def authorize_url(client, params) do
+    AuthCode.authorize_url(client, params)
+  end
+
+  def get_token(client, params, headers) do
+    client
+    |> put_header("Accept", "application/json")
+    |> AuthCode.get_token(params, headers)
+  end
+end
--- github_trends/web/router.ex
+++ github_trends/web/router.ex
@@ -19,6 +19,13 @@ defmodule GithubTrends.Router do
     get "/", PageController, :index
   end
 
+  scope "/auth", GithubTrends do
+    pipe_through :browser # Use the default browser stack
+    get "/:provider", AuthController, :index
+
+    get "/:provider/callback", AuthController, :callback
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", GithubTrends do
   #   pipe_through :api
