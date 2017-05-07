52752cc4bca9f7db987eebb321f3dae8cfe53779
Patryk Mrukot <patryk.mrukot@schibsted.pl>
2017-05-07 20:13:24 +0200

    feat(web): Add support for query params

--- github_trends/web/controllers/github_search_controller.ex
+++ github_trends/web/controllers/github_search_controller.ex
@@ -5,18 +5,47 @@ defmodule GithubTrends.GithubSearchController do
   HTTPGithub.start
 
   @expected_fields ~w(
-      stargazers_count watchers full_name description language html_url forks_count
+    stargazers_count watchers full_name description language html_url forks_count
   )
 
   def get_most_popular_repositories(conn, params) do
-    IO.inspect params
-    %{body: body} = HTTPGithub.get!("search/repositories?q=language:javascript%20created:%3E2017-05-01&sort=stars&order=desc")
+    q_param =
+      case Map.take(params, ["language", "created"]) do
+        %{"language" => language, "created" => created} ->
+          "language:" <> language <> " created:>" <> created
+        %{"language" => language} ->
+          "language:" <> language
+        %{"created" => created} ->
+          "created:>" <> created
+        _ ->
+          nil
+      end
 
-    case body[:items] do
+    params =
+      case q_param do
+        nil ->
+          params
+          |> Map.take(["sort", "order"])
+        _ ->
+          params
+          |> Map.take(["sort", "order"])
+          |> Map.put("q", q_param)
+      end
+
+    request = HTTPGithub.get!("search/repositories", [], params: params)
+
+    data =
+      case request do
+        %{body: [items: items, total_count: total_count], headers: headers} -> items
+        _ -> []
+      end
+
+    case data do
       data ->
         data = Enum.map data, fn x -> Map.take x, @expected_fields end
         json conn, data
-      _ -> "Page not found"
+      [] ->
+        json conn, []
     end
 
   end
