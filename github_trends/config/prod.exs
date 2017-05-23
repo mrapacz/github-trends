37251874309f079d5ff9aa4ab10c4e7a2e33a99f
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-05-23 22:33:42 +0200

    Add force_ssl and change scheme to https

--- github_trends/config/prod.exs
+++ github_trends/config/prod.exs
@@ -2,7 +2,8 @@ use Mix.Config
 
 config :github_trends, GithubTrends.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "http", host: "githubtrends.herokuapp.com", port: 80],
+  url: [scheme: "https", host: "githubtrends.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
