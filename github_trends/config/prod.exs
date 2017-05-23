4baaff63f9dc78073340f781abc713f1af0df62b
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-05-23 22:58:18 +0200

    Move to http scheme

--- github_trends/config/prod.exs
+++ github_trends/config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :github_trends, GithubTrends.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "githubtrends.herokuapp.com", port: 443],
+  url: [scheme: "http", host: "githubtrends.herokuapp.com", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
