c62c99e654909dbb04fd4e519d76f3cce7d0bf32
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-05-23 22:58:41 +0200

    remove force ssl

--- github_trends/config/prod.exs
+++ github_trends/config/prod.exs
@@ -3,7 +3,6 @@ use Mix.Config
 config :github_trends, GithubTrends.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "http", host: "githubtrends.herokuapp.com", port: 80],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 
