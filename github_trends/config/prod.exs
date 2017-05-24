eecd3a4383f2f21ec15285e25306a8c1dbbac7f0
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-05-24 10:10:37 +0200

    Add manifest.json

--- github_trends/config/prod.exs
+++ github_trends/config/prod.exs
@@ -3,6 +3,7 @@ use Mix.Config
 config :github_trends, GithubTrends.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "http", host: "githubtrends.herokuapp.com", port: 80],
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 
