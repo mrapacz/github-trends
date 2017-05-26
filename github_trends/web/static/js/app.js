1bfd708eef92caf8aad8129e86bbd06f2165af78
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-05-26 19:48:06 +0200

    Add protocol prefix to host injection

--- github_trends/web/static/js/app.js
+++ github_trends/web/static/js/app.js
@@ -20,6 +20,6 @@ import "phoenix_html";
 
 // import socket from "./socket"
 // Set up our Elm App
-var host = window.location.host;
+var host = window.location.protocol + "//" + window.location.host;
 const elmDiv = document.querySelector('#elm-container');
 const elmApp = Elm.App.embed(elmDiv, {host: host});
